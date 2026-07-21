de709c937d9cf578e91448a400242bf31756a6c6
Damilola Samuel Adesina <136617797+damilola002@users.noreply.github.com>
2026-07-21 03:00:18 +0000

    ...

--- CampusCommute/user/views.py
+++ CampusCommute/user/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, AuthenticationForm, login
 from .models import CustomerUser
+from django.contrib.auth.models import User
 
 def login_user(request):
     if request.method == "POST":
@@ -20,4 +21,17 @@ def login_user(request):
             
     return render(request, 'login.html')
     #bloom filter algotithim can be used for usernames and emails (anything unique), bit field 
- 
+def register_user(request):
+    email_entered = request.POST['email']
+    password_entered = request.POST['password']
+    username_entered = request.POST['username']
+        
+    user = create_user(request, email=email_entered, username = username_entered, password=password_entered)
+        
+    if user is not None:
+        user(request, user)
+        return redirect('dashboard')
+    else:
+        return render(request, 'register.html', {'error': 'Invalid credentials'})
+            
+    return render(request, 'login.html')
