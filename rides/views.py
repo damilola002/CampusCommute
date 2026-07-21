5dc1adb6085924c54c0de0bebac40a682597a4b6
Maxine Mugi <mmm1135@mavs.uta.edu>
2026-07-21 16:02:29 -0500

    added a success message and made the page look more appealing

--- rides/views.py
+++ rides/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Ride
+from django.contrib import messages
 
 def offer_ride(request):
     if request.method == "POST":
@@ -10,5 +11,6 @@ def offer_ride(request):
             time = request.POST["time"],
             seats_available = request.POST["seats_available"]
         )
+        messages.success( request, "Your ride offer has been posted!!")
     return render (request,
                    "offer_ride.html")
