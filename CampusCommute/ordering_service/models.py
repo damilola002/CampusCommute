1596bc1a92a8c4413741b9eb0e78a2df1e29fd5c
Damilola Samuel Adesina <136617797+damilola002@users.noreply.github.com>
2026-07-22 23:56:59 +0000

    ...

--- CampusCommute/ordering_service/models.py
+++ CampusCommute/ordering_service/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings 
 
 # Create your models here.
+class Ordering(models.Model):
+
+    PENDING  = "PP"
+    ACCEPTED = "AA"
+    CANCELLED = "CC"
+    STATUS_CHOICES = {
+        PENDING: "Waiting on driver",
+        ACCEPTED: "Accepted",
+        CANCELLED: "Cancelled",
+    }
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #create for driver
+    #class Status(models.)
+    
--- CampusCommute/ordering_service/views.py
+++ CampusCommute/ordering_service/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+#creating the orders 
+def orders(request):
+    if request.method == "GET":
+        user_order = request.POST.get('pickup_location')
